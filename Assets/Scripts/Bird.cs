f872409ea0fb516e33e824ec6ef2a9a28fcc3d1a
itspaulthegreat <arindam.8599@gmail.com>
2021-03-09 23:49:59 +0530

    up

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    Vector2 _startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = GetComponent<Rigidbody2D>().position;
+        GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+ 
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(255,255,0);
+
+    }
+     void OnMouseUp()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        Vector2 currentpos = GetComponent<Rigidbody2D>().position;
+        Vector2 direction =  _startPos- currentpos;
+        direction.Normalize();
+        GetComponent<Rigidbody2D>().AddForce(direction*500);
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+    }
+   
+    void OnMouseDrag()
+    {
+        // // Vector2 position = ;
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GetComponent<Transform>().position  = new Vector3(pos.x,pos.y,transform.position.z);
+        
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
